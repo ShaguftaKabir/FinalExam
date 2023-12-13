4874e0feb17c366f6fb140501462fea07e4bd8a7
shaguftakabir <shagufta01.kabir@gmail.com>
2023-12-13 11:50:18 +0600

    third commit

--- final.js
+++ final.js
@@ -1,21 +1,25 @@
-function show(
+function show(){
     var searchTerm= document.getElementById("searchBox").value;
-
-    var url= `https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid=${searchTerm}`;
+    var apiKey= '5393348a84e007a9a7bfe365a3f94e17'
+    var url= `https://api.openweathermap.org/data/2.5/weather?q=${searchTerm}&appid=${apiKey}`;
 
     fetch (url)
-    .then(res =. res.json())
+    .then(res => res.json())
     .then(data => display(data))
-)
+}
 function display(data){
-    var allData= data.coord;
+    var allData= data.;
     var oldContent= document.getElementById("content");
     oldContent.textContent= "";
     console.log(allData);
-}
+
 for(var i=0; i<allData.length; i++){
     var newDiv= document.createElement("div");
-    newDiv.innerHTML= ``
+    newDiv.innerHTML= `Name: ${allData[i].name} <br>
+                       Temperature: ${allData[i].temp} <br>
+                       `;
+
+
     newDiv.classList.add("innerStyle");
     oldContent.appendChild(newDiv);
 }
